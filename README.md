eabd317c7056d573e5d680d221446eae081bdd78
David Keller <mrcodeswildride@gmail.com>
2021-03-10 19:41:12 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# expression-parser
+
+Expression parser web app written in HTML, CSS, and JavaScript.
+
+Use this app at [expressionparser.mrcodeswildride.com](https://expressionparser.mrcodeswildride.com/).
+Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
---
+++ script.js
@@ -0,0 +1,112 @@
+let expression = document.getElementById(`expression`)
+let parseButton = document.getElementById(`parseButton`)
+let answerParagraph = document.getElementById(`answerParagraph`)
+
+parseButton.addEventListener(`click`, parseExpression)
+
+expression.addEventListener(`keydown`, keyPressed)
+expression.focus()
+
+function parseExpression() {
+  let expressionValue = expression.value.replaceAll(` `, ``)
+  let answer = evalExpression(expressionValue)
+
+  if (!isNaN(answer)) {
+    answerParagraph.innerHTML = answer
+  }
+  else {
+    answerParagraph.innerHTML = `Invalid input.`
+  }
+}
+
+function keyPressed(event) {
+  if (event.keyCode == 13) {
+    parseExpression()
+  }
+}
+
+function evalExpression(expression) {
+  let obj = extractTermsOperators(expression)
+
+  evalTerms(obj.terms)
+
+  return applyOperators(obj.terms, obj.operators)
+}
+
+function extractTermsOperators(expression) {
+  let terms = []
+  let operators = []
+
+  let parenCount = 0
+  let term = ``
+
+  for (let character of expression) {
+    if (character == `(`) {
+      parenCount++
+    }
+    else if (character == `)`) {
+      parenCount--
+    }
+
+    if (parenCount == 0 && `+-*/`.includes(character)) {
+      terms.push(term)
+      operators.push(character)
+
+      term = ``
+    }
+    else {
+      term += character
+    }
+  }
+
+  terms.push(term)
+
+  return {
+    terms: terms,
+    operators: operators
+  }
+}
+
+function evalTerms(terms) {
+  for (let i = 0; i < terms.length; i++) {
+    if (terms[i] == ``) {
+      terms[i] = NaN
+    }
+    else if (!isNaN(terms[i])) {
+      terms[i] = Number(terms[i])
+    }
+    else {
+      let firstChar = terms[i][0]
+      let lastChar = terms[i][terms[i].length-1]
+
+      if (firstChar != `(` || lastChar != `)`) {
+        terms[i] = NaN
+      }
+      else {
+        let subexpression = terms[i].substring(1, terms[i].length-1)
+        terms[i] = evalExpression(subexpression)
+      }
+    }
+  }
+}
+
+function applyOperators(terms, operators) {
+  let answer = terms[0]
+
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] == `+`) {
+      answer += terms[i+1]
+    }
+    else if (operators[i] == `-`) {
+      answer -= terms[i+1]
+    }
+    else if (operators[i] == `*`) {
+      answer *= terms[i+1]
+    }
+    else if (operators[i] == `/`) {
+      answer /= terms[i+1]
+    }
+  }
+
+  return answer
+}
